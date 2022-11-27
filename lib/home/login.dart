1b4f4fedebfb0690e9915e113847ce54b0f9e36a
radityahnf <ferdar7@gmail.com>
2022-11-27 12:26:12 +0700

    nyoba add login

---
+++ lib/home/login.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:travelogue/home/home.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _loginFormKey = GlobalKey<FormState>();
+  Color buttonColor = Color.fromRGBO(254, 185, 0, 100);
+
+  String username = "";
+  String password1 = "";
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    // The rest of your widgets are down below
+    // 'username' and 'password' should be the values of the user login form.
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/logo.png",
+              height: 70,
+              width: 70,
+            ),
+            Form(
+              key: _loginFormKey,
+              child: SingleChildScrollView(
+                  child: Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          "Login to TraveLoGue",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                              color: Color.fromARGB(178, 3, 3, 3)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Username ",
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 12),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                          ),
+                          // Menambahkan behavior saat nama diketik
+                          onChanged: (String? value) {
+                            setState(() {
+                              username = value!;
+                            });
+                          },
+                          // Menambahkan behavior saat data disimpan
+                          onSaved: (String? value) {
+                            setState(() {
+                              username = value!;
+                            });
+                          },
+                          // Validator sebagai validasi form
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please fill out this field.";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        // Menggunakan padding sebesar 8 pixels
+                        padding: const EdgeInsets.all(8.0),
+
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Password ",
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 12),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                          ),
+                          // Menambahkan behavior saat nama diketik
+                          onChanged: (String? value) {
+                            setState(() {
+                              password1 = value!;
+                            });
+                          },
+                          // Menambahkan behavior saat data disimpan
+                          onSaved: (String? value) {
+                            setState(() {
+                              password1 = value!;
+                            });
+                          },
+                          // Validator sebagai validasi form
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please fill out this field.";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(14))),
+                            backgroundColor:
+                                MaterialStateProperty.all(buttonColor),
+                          ),
+                          onPressed: () async {
+                            final response = await request
+                                .login("http://127.0.0.1:8000/auth/login/", {
+                              'username': username,
+                              'password': password1,
+                            });
+
+                            if (response.loggedIn) {
+                              if (_loginFormKey.currentState!.validate()) {
+                                Navigator.pushReplacementNamed(
+                                    context, '/home');
+                              }
+                            }
+                          },
+                          // onPressed: (() {
+                          //   if (_loginFormKey.currentState!.validate()) {
+                          //     Navigator.pushReplacement(
+                          //         context,
+                          //         MaterialPageRoute(
+                          //           builder: (context) => const HomePage(),
+                          //         ));
+                          //   }
+                          // }),
+                        ),
+                      ),
+                    ]),
+              )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
