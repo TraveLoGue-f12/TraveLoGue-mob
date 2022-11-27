e33dd5f9588d2d7cd957e75f8db547a5e10b4865
ahmad.fatahillah <ahmadfatahillah16@gmail.com>
2022-11-27 22:20:11 +0700

    add attractions module

---
+++ attractions/lib/attractions.dart
@@ -0,0 +1,19 @@
+library attractions;
+
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+
+class Attractions extends StatefulWidget {
+  const Attractions({super.key});
+
+  @override
+  State<Attractions> createState() => _AttractionsState();
+}
+
+class _AttractionsState extends State<Attractions> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
