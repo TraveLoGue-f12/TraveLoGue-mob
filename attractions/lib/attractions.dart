0632ae7298941b3f0c0c05fe282ebfc7b4d4f898
ahmad.fatahillah <ahmadfatahillah16@gmail.com>
2022-12-10 17:19:17 +0700

    pull staging

--- attractions/lib/attractions.dart
+++ attractions/lib/attractions.dart
@@ -1,6 +1,7 @@
 library attractions;
 
-import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 
 class Attractions extends StatefulWidget {
